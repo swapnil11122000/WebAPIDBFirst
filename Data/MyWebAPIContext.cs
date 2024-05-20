b95362d4f7091b18abb1b4a7097534e4e52de3dd
Swapnil Santosh Muluk <swapnilmuluk6@gmail.com>
2024-05-20 22:57:06 +0530

    Add project files.

---
+++ Data/MyWebAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyWebAPI.Model;
+
+namespace MyWebAPI.Data
+{
+    public class MyWebAPIContext : DbContext
+    {
+        public MyWebAPIContext (DbContextOptions<MyWebAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyWebAPI.Model.Book> Book { get; set; } = default!;
+    }
+}
